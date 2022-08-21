dfba8e7ad595468987f7fea2bb9d4c4df336e0f7
Fatma Elzahraa Atef <fatmaatef8720@gmail.com>
2022-08-21 07:28:31 +0200

    lab4

---
+++ ITI Lab4/ITI Lab4/Employee.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI_Lab4
+{
+    struct Date
+    {
+      
+        private int day;
+        private int month;
+        private int year;
+        public Date(int day, int month, int year)
+        {
+            this.day = day;
+            this.month = month;
+            this.year = year;
+        }
+
+        public int GetDay()
+        {
+            return day;
+        }
+        public void SetDay(int d)
+        {
+            day = d;
+        }
+
+        public int GetMonth()
+        {
+            return month;
+        }
+        public void SetMonth(int m)
+        {
+            month = m;
+        }
+ 
+        public int Getyear()
+        {
+            return year;
+        }
+        public void SetYear(int y)
+        {
+            year = y;
+        }
+
+    }
+     public enum Gender
+    {
+        Female,
+        Male,
+    };
+     public enum SecurityLevel
+    {
+        guest,
+        Developer,
+        secretary,
+        DBA
+    };
+     struct Employee
+     {
+        private int ID;
+        public int GetID()
+        {
+            return ID;
+        }
+        public void SetID(int id)
+        {
+            ID = id;
+        }
+
+         
+        private decimal Salary;
+        public decimal GetSalary()
+        {
+            return Salary;
+        }
+        public void SetSalary(decimal s)
+        {
+            Salary = s;
+        }
+
+
+        private Date HireDate;
+        public Date GetHireDate()
+        {
+            return  HireDate;
+        }
+        public void SetHireDate(Date d)
+        {
+            HireDate = d;
+        }
+
+        private Gender gender;
+        public Gender GetGender()
+        {
+            return gender;
+        }
+        public void SetGendery(Gender gen)
+        {
+                gender = gen;
+          
+        }
+
+
+        private SecurityLevel securityLevel;
+        public SecurityLevel GetsecurityLevel()
+        {
+            return securityLevel;
+        }
+        public void SetsecurityLevel(SecurityLevel SL)
+        {
+            securityLevel = SL;
+        }
+
+
+        public void DisplayData()
+        {
+            Console.WriteLine($"ID: {ID}\t\tGender : {gender}\t\tSecurity Level :{securityLevel}\t\tSalary: {Salary} EGP\t\tHire Date : {HireDate.GetDay()} / {HireDate.GetMonth()} / {HireDate.Getyear()}");
+
+        }
+    }
+}
---
+++ ITI Lab4/ITI Lab4/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+
+namespace ITI_Lab4
+{
+
+    class Program
+    {
+        //public static void Add_Employee(Employee e,int ID, decimal salary, Gender gender, SecurityLevel securityLevel, Date HireDate)
+        //{
+        //    e.SetID(ID);
+        //    e.SetSalary(salary);
+        //    e.SetGendery(gender);
+        //    e.SetsecurityLevel(securityLevel);
+        //    e.SetHireDate(HireDate);
+
+        //    Console.WriteLine("Employee Add");
+        //}
+        static void Main(string[] args)
+        {
+
+            Employee[] employees = new Employee[3];
+            for (int i = 0; i < 3; i++)
+            {
+                #region ID
+                int ID=0;
+
+                while (true)
+                {
+                    Console.WriteLine($"\nEnter employee {i + 1} ID : ");
+
+                    if (int.TryParse(Console.ReadLine(), out ID))
+                    {
+                        break;
+                    }
+                    Console.WriteLine($"\n\t\t\tInValid employee ID ");
+
+                }
+
+                // while (!int.TryParse(Console.ReadLine(), out ID));
+
+                #endregion
+
+                #region salary
+                decimal salary;
+                while (true)
+                {
+                    Console.WriteLine($"\nEmployee {i + 1} Salary : ");
+
+                    if (decimal.TryParse(Console.ReadLine(), out salary))
+                    {
+                        break;
+                    }
+                    Console.WriteLine($"\n \t\t\tNot Valid employee Salary ");
+
+                }
+
+                #endregion
+
+                #region gender
+
+                string gen;
+                Gender gender = new Gender();
+
+                while (true){
+
+                    Console.WriteLine($"\n\nEnter Employee {i + 1} Gender  :   Male\\Female or APPR F\\M");
+
+                    gen = Console.ReadLine();
+                    if (gen.ToUpper() == "F" || gen.ToUpper() == "FEMALE")
+                    {
+                        gender = Gender.Female;
+                        break;
+                    }
+                    else if (gen.ToUpper() == "M" || gen.ToUpper() == "MALE")
+                    {
+                        gender = Gender.Male;
+                        break;
+                    }
+                    else Console.WriteLine($"\n\t\t\tInValid Gender\n");
+
+                }
+
+
+                #endregion
+
+                string SL;
+                SecurityLevel securityLevel = new SecurityLevel();
+
+                #region securityLevel
+                while (true)
+                {
+
+                    Console.WriteLine($"\nEmployee {i + 1} Security Level : \nGuest  : \t\t1 \nDeveloper  : \t\t2 \nsecretary  : \t\t3 \nDBA  : \t\t\t4 \n ");
+                    SL = Console.ReadLine();
+
+                    if (SL.ToUpper() == "GUEST" || SL.ToUpper() == "1")
+                    {
+                        securityLevel = SecurityLevel.guest;
+                        break;
+                    }
+                    else if (SL.ToUpper() == "Developer" || SL.ToUpper() == "2")
+                    { 
+                        securityLevel = SecurityLevel.Developer;
+                        break;
+                    }
+                    else if (SL.ToUpper() == "SECRETARY" || SL.ToUpper() == "3")
+                    { 
+                        securityLevel = SecurityLevel.secretary;
+                        break;
+                    }
+                    else if (SL.ToUpper() == "DBA" || SL.ToUpper() == "4") 
+                    { 
+                        securityLevel = SecurityLevel.DBA;
+                        break;
+                    }
+                    else Console.WriteLine($"\t\t\tInValid Security Level ");
+                     
+                }
+                #endregion
+
+                #region date
+                int day, year, month;
+                bool g;
+
+                    Console.WriteLine($"\nemployee {i + 1} Hire Date :\tDay - Month - Year   - Each one in new line - ");
+                    bool dc = false, mc=false, yc=false;
+                    
+                        while (true)
+                        {
+                            Console.WriteLine($"\nEmployee {i + 1} Hire Date :\tDay : ");
+
+                            if (int.TryParse(Console.ReadLine(), out day) && (day >= 1 && day <= 31))
+                            {
+                                dc = true;
+                                break;
+                            }
+                            Console.WriteLine($"\n\t\t\tInValid Day ");
+                        }
+                        while (true)
+                        {
+                            Console.WriteLine($"\nEmployee {i + 1} Hire Date :\tMonth : ");
+
+                            if (int.TryParse(Console.ReadLine(), out month) && (month >= 1 && month <= 12))
+                            {
+                                break;
+                                mc = true;
+                            }
+                            Console.WriteLine($"\n\t\t\tInValid Month ");
+                        }
+                        while (true)
+                        {
+                            Console.WriteLine($"\nEmployee {i + 1} Hire Date :\tYear : ");
+
+                            if (int.TryParse(Console.ReadLine(), out year) && (year >= 2006 && year <= 2022))
+                            {
+                                mc = true;
+                                break;
+                            }
+                            Console.WriteLine($"\n\t\t\tInValid Year ");
+                        }
+
+
+                Date HireDate = new Date(day,month,year);
+                #endregion
+
+                employees[i].SetID(ID);
+                employees[i].SetSalary(salary);
+                employees[i].SetGendery(gender);
+                employees[i].SetsecurityLevel(securityLevel);
+                employees[i].SetHireDate(HireDate);
+                Console.WriteLine("\n\t\t --------------    Employee Add    -------------\t\t\n");
+                //Add_Employee(employees[i],ID,salary,gender,securityLevel,HireDate);
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                employees[i].DisplayData();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
